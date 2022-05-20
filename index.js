679f80ae66a4f55d09d77ed7b17dbfafe13df45f
Neamul01 <neamulhaque2002@gmail.com>
2022-05-21 01:53:39 +0600

    route problem solved

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
             res.send(users)
         })
 
-        app.get('admin/:email', async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
